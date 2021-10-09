749c1a30b07391a7a4365891e684e3d567fd0501
Pasindu P Konghawaththa <pasindudixon25@gmail.com>
2021-10-09 10:46:59 +0530

    create customer section close #1

--- src/controller/CustomerViewController.java
+++ src/controller/CustomerViewController.java
@@ -1,4 +1,30 @@
 package controller;
 
+import db.DBConnection;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import javafx.event.ActionEvent;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class CustomerViewController {
+    public TextField txtCustomerName;
+    public TextField txtCustomerAddress;
+    public Button btnAdd;
+
+    public void AddBthOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        System.out.println("Hello");
+            String name=txtCustomerName.getText();
+            String address=txtCustomerAddress.getText();
+        Connection connection = DBConnection.getInstance().getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into Customer(?,?)");
+        preparedStatement.setString(1,name);
+        preparedStatement.setString(2,address);
+        boolean execute = preparedStatement.execute();
+        System.out.println(execute);
+
+    }
 }
---
+++ src/entity/Customer.java
@@ -0,0 +1,19 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
+public class Customer {
+    String name;
+    String address;
+}
